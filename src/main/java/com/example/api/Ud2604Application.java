4c7fc182002d465145a12822aae92b7e6292a3ef
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-22 13:11:18 +0200

    Initial commit

---
+++ src/main/java/com/example/api/Ud2604Application.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud2604Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud2604Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api/Ud2604ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ud2604ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
