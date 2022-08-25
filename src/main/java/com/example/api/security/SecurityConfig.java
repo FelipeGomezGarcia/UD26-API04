f4ec635434daf59186e0d5a5b4bff54b3acbf078
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-25 16:13:16 +0200

    Capa JWT

---
+++ src/main/java/com/example/api/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.api.security;
+
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+	
+		http.csrf().disable().authorizeRequests()
+        .antMatchers("/login").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .addFilterBefore(new LoginFilter("/login", authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class)
+
+        .addFilterBefore(new JwtFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("admin")
+		.password("{noop}admin")
+		.roles("ADMIN");
+		
+		auth.inMemoryAuthentication()
+		.withUser("felipe")
+		.password("{noop}felipe")
+		.roles("USER");
+	}
+}
